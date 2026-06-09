fa284785eaec77c3b7fedeafcf3ceb5577f4ebc6
PumpApi <themoongotogo@gmail.com>
2026-06-09 14:07:06 +0300

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -9,7 +9,7 @@ after=$(git rev-parse HEAD)
 if [ "$before" != "$after" ]; then
 /root/pumpapi-agent/venv/bin/pip install -r backend/requirements.txt
 systemctl restart pumpapi-agent.service
-hermes update -y
-systemctl restart hermes-gateway
+#hermes update -y
+#systemctl restart hermes-gateway
 python3 update_hermes_soul_and_skills.py
 fi
